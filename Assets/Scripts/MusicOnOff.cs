74242de75de24074808b27917a7de1ef9e41850f
duyquangbui99 <77463586+duyquangbui99@users.noreply.github.com>
2021-02-27 22:09:28 -0600

    Add-Sound to Frogger

---
+++ Assets/Scripts/MusicOnOff.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class MusicOnOff : MonoBehaviour
+{
+    public Text musicText;
+    public AudioClip gameMusic;
+    public AudioSource audiosource;
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void pauseMusic()
+    {
+        audiosource.Pause();
+    }
+
+    public void resumeMusic()
+    {
+        audiosource.UnPause();
+    }
+}
